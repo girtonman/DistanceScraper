4f7028c831aa96e7c89377e3c2944b2fcb3404c9
Scott Girton <girtonman@gmail.com>
2023-12-06 20:00:51 -0600

    Add a `Verbose` setting for logs
    Will be used to determine whether to do extra logging that might be slower, or to log less information and run faster.

--- DistanceScraper/Settings.cs
+++ DistanceScraper/Settings.cs
@@ -5,5 +5,6 @@
 		public static string ConnectionString { get; set; }
 		public static string Username { get; set; }
 		public static string Password { get; set; }
+		public static bool Verbose { get; set; } = false;
 	}
 }
