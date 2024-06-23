ca7d50af58a6fa42dede7aa9784f5eea954c73a0
Scott Girton <girtonman@gmail.com>
2024-06-22 19:07:18 -0500

    Add rate limiting to SteamAPIDAL
    Also refactors some of the code to dry it up a bit.

--- DistanceScraper/DALs/SteamAPIDAL.cs
+++ DistanceScraper/DALs/SteamAPIDAL.cs
@@ -6,76 +6,132 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
+using System.Threading;
 
 namespace DistanceTracker.DALs
 {
-	public class SteamDAL
+	public static class SteamAPIDAL
 	{
-		public SteamDAL()
+		private static string SteamAPIKey { get; set; }
+		private static Uri BaseAPIAddress { get; set; }
+		private static Task ResetBucketTask;
+		private static int BucketCount = 0;
+
+		public static void Init()
 		{
 			SteamAPIKey = Settings.SteamAPIKey;
+			BaseAPIAddress = new Uri("https://api.steampowered.com");
+			ResetBucketTask = ResetBucket();
+			ResetBucketTask.Start();
 		}
 
-		private string SteamAPIKey { get; set; }
+		private static async Task ResetBucket()
+		{
+			await Task.Delay(Settings.SteamAPIBucketingWindowSeconds * 1000);
+			Utils.WriteLine("SteamAPI", $"Resetting rate limiting bucket. API calls this window: {BucketCount}");
+			Volatile.Write(ref BucketCount, 0);
+			ResetBucketTask = ResetBucket();
+			ResetBucketTask.Start();
+		}
+
+		private HttpClient CreateClient()
+		{
+			return new HttpClient
+			{
+				BaseAddress = BaseAPIAddress
+			};
+		}
+
+		private static async Task RateLimit()
+		{
+			if (Volatile.Read(ref BucketCount) > Settings.SteamAPIWindowLimit)
+			{
+				await ResetBucketTask;
+			}
+			Interlocked.Increment(ref BucketCount);
+		}
+
+		private async Task<JObject> RateLimitedPost(HttpClient client, string requestURI, List<KeyValuePair<string, string>> parameters = null)
+		{
+			await RateLimit();
+
+			FormUrlEncodedContent content = null;
+			if (parameters != null)
+			{
+				parameters.Append(new KeyValuePair<string, string>("key", SteamAPIKey));
+				content = new FormUrlEncodedContent(parameters);
+			}
+
+			var response = await client.PostAsync(requestURI, content);
+			var responseMessage = await response.Content.ReadAsStringAsync();
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new Exception($"Steam API call failed ({response.StatusCode}): {responseMessage}");
+			}
+
+			return JObject.Parse(responseMessage);
+		}
+
+		private async Task<JObject> RateLimitedGet(HttpClient client, string endpoint, Dictionary<string, object> parameters = null)
+		{
+			await RateLimit();
+
+			string parametersURI = "";
+			if (parameters != null)
+			{
+				parameters.Append(new KeyValuePair<string, object>("key", SteamAPIKey));
+				parametersURI = "?" + string.Join("&", parameters.Select(x => $"{x.Key}={HttpUtility.UrlEncode(x.Value.ToString())}"));
+			}
+
+			var response = await client.GetAsync(endpoint + parametersURI);
+			var responseMessage = await response.Content.ReadAsStringAsync();
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new Exception($"Steam API call failed ({response.StatusCode}): {responseMessage}");
+			}
+
+			return JObject.Parse(responseMessage);
+		}
 
 		public async Task<PublishedFileDetail> GetWorkshopInfo(uint fileID)
 		{
-			var client = new HttpClient
+			var client = CreateClient();
+			var parameters = new List<KeyValuePair<string, string>>()
 			{
-				BaseAddress = new Uri("http://api.steampowered.com")
+				new("itemcount", "1"),
+				new("publishedfileids[0]", $"{fileID}"),
 			};
-			var content = new FormUrlEncodedContent(new[]
-			{
-				new KeyValuePair<string, string>("key", SteamAPIKey),
-				new KeyValuePair<string, string>("itemcount", "1"),
-				new KeyValuePair<string, string>("publishedfileids[0]", $"{fileID}"),
-			});
-			var response = await client.PostAsync("ISteamRemoteStorage/GetPublishedFileDetails/v1/", content);
 
-			var responseMessage = await response.Content.ReadAsStringAsync();
+			var json = await RateLimitedPost(client, "ISteamRemoteStorage/GetPublishedFileDetails/v1/", parameters);
 
-			var json = JObject.Parse(responseMessage);
-			var workshopDetails = json["response"]["publishedfiledetails"].ToObject<List<PublishedFileDetail>>()[0];
-
-			return workshopDetails;
+			return json["response"]["publishedfiledetails"].ToObject<List<PublishedFileDetail>>()[0];
 		}
 
 		public async Task<List<PublishedFileDetail>> GetWorkshopLevelList(uint mostRecentFileID = 0)
 		{
 			// Setup client and reusable part of the uri
-			var client = new HttpClient
+			var client = CreateClient();
+			var parameters = new Dictionary<string, object>
 			{
-				BaseAddress = new Uri("https://api.steampowered.com")
+				{"query_type", 1},
+				{"numperpage", 100},
+				{"appid", 233610},
+				{"return_details", true}
 			};
-			var content = new Dictionary<string, string>
-			{
-				{"key", SteamAPIKey},
-				{"query_type", "1"},
-				{"numperpage", "100"},
-				{"appid", "233610"},
-				{"return_details", "true"},
-			};
-			var endpoint = "/IPublishedFileService/QueryFiles/v1/?";
-			var uri = endpoint + string.Join("&", content.Select(x => x.Key + "=" + x.Value));
+			var endpoint = "/IPublishedFileService/QueryFiles/v1/";
 
 			// Loop through results until the end of the list or until we see the most recent fileID that we already know about
 			var keepScanning = true;
-			var cursor = "*"; // First cursor should be "*", then subsequently the value of "next_cursor" from the response
+			// First cursor should be "*", then subsequently the value of "next_cursor" from the response
+			var cursor = "*";
 			var levelList = new List<PublishedFileDetail>();
 			while (keepScanning)
 			{
+				// Update the cursor
+				parameters["cursor"] = cursor;
+				
 				// Query the API for workshop levels
-				var response = await client.GetAsync(uri + $"&cursor={HttpUtility.UrlEncode(cursor)}");
-				var responseMessage = await response.Content.ReadAsStringAsync();
-
-				if (!response.IsSuccessStatusCode)
-				{
-					throw new Exception($"Aborting workshop level retrieval: {responseMessage}");
-				}
-
-				// Attempt to parse the response into a json object
-				JObject json = null;
-				json = JObject.Parse(responseMessage);
+				var json = await RateLimitedGet(client, endpoint, parameters);
 
 				// Get the next cursor
 				var nextCursor = json["response"]["next_cursor"].ToString();
@@ -112,37 +168,20 @@ namespace DistanceTracker.DALs
 
 		public async Task<List<PlayerSummary>> GetPlayerSummaries(List<ulong> steamIDs, int workerNumber)
 		{
-			// Setup client and reusable part of the uri
-			var client = new HttpClient
+			// Setup client and the reusable part of the uri
+			var client = CreateClient();
+			var parameters = new Dictionary<string, object>
 			{
-				BaseAddress = new Uri("https://api.steampowered.com")
-			};
-			var content = new Dictionary<string, string>
-			{
-				{"key", SteamAPIKey},
 				{"steamids", string.Join(",", steamIDs)},
 			};
 			var endpoint = "/ISteamUser/GetPlayerSummaries/v2/?";
-			var uri = endpoint + string.Join("&", content.Select(x => x.Key + "=" + x.Value));
-
-			var players = new List<PlayerSummary>();
 
 			// Query the API for player summaries
-			var response = await client.GetAsync(uri);
-			var responseMessage = await response.Content.ReadAsStringAsync();
-
-			if (!response.IsSuccessStatusCode)
-			{
-				throw new Exception($"Aborting player summary retrieval: {responseMessage}");
-			}
-
-			// Attempt to parse the response into a json object
-			JObject json = null;
-			json = JObject.Parse(responseMessage);
+			var json = await RateLimitedGet(client, endpoint, parameters);
 
 			// Collect player summaries
 			// Breaking this up because ToObject<List<PlayerSummary>> was misbehaving
-			var batch = new List<PlayerSummary>();
+			var players = new List<PlayerSummary>();
 			foreach (var jToken in json["response"]["players"])
 			{
 				var player = jToken.ToObject<PlayerSummary>();
